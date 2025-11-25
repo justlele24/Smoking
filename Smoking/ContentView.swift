e09a2a71080f5c20d35f402a4b5fd97d948d8297
justlele24 <lele.barra2403@gmail.com>
2025-11-25 10:56:32 +0100

    Initial Commit

---
+++ Smoking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Smoking
+//
+//  Created by Raffaele Barra on 25/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Smoking/SmokingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmokingApp.swift
+//  Smoking
+//
+//  Created by Raffaele Barra on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SmokingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
