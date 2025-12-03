2d7b8cc8d4fad3a3094ce4701e1f4feb7bdb377f
justlele24 <lele.barra2403@gmail.com>
2025-12-03 09:49:18 +0100

    bundle identifier

---
+++ Smoking/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Smoking
+//
+//  Created by Raffaele Barra on 03/12/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
